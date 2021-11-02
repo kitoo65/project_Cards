0b5ecb9bbf0961123a9565c540c406edf16837d9
kitoo65 <35207880+kitoo65@users.noreply.github.com>
2021-11-02 14:29:31 -0300

    First Commitment in GIT

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+
+	cards := newDeck()
+	cards.print()
+
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+
+	remainingCards.print()
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
+
+func (d deck) toString() string { //Its going to take a deck and return a complete string representation of it
+
+}
